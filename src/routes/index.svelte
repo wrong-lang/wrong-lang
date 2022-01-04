4fa407fe31878e0c904bf212f04ad0f19780b663
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-01-04 17:54:51 +0700

    Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<div class="grid h-screen place-items-center">
+    <div class="container">
+        <div class="flex flex-row justify-center">
+            <img src="https://raw.githubusercontent.com/sveltejs/branding/master/svelte-logo.png" alt="svelte" width="400">
+            <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.79614a5f61617ba49a0891494521226b.svg" alt="tailwind" width="400">
+        </div>
+        <h1 class="text-5xl text-center font-light text-black">
+            Welcome to
+            <span class="text-orange-600/80">Svelte Kit</span> + <span class="text-blue-600/80">TailwindCSS</span>
+        </h1>
+    </div>
+</div>
